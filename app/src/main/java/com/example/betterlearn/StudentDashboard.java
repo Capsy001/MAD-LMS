b4fa654930fe8e0c52f6d738bd5a5c63fd74ab49
Capsy001 <uvenfast@gmail.com>
2021-08-14 22:34:18 +0530

    Added dashboard interfaces-onclick

--- app/src/main/java/com/example/betterlearn/StudentDashboard.java
+++ app/src/main/java/com/example/betterlearn/StudentDashboard.java
@@ -1,8 +1,12 @@
 package com.example.betterlearn;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class StudentDashboard extends AppCompatActivity {
 
@@ -11,4 +15,7 @@ public class StudentDashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_dashboard);
     }
+
+
+
 }
--- app/src/main/java/com/example/betterlearn/Tabs.java
+++ app/src/main/java/com/example/betterlearn/Tabs.java
@@ -3,21 +3,48 @@ package com.example.betterlearn;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
-public class Tabs extends Fragment {
+public class Tabs extends Fragment implements View.OnClickListener {
 
-
+    Button myButton;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        View myView = inflater.inflate(R.layout.fragment_tabs, container, false);
+        myButton = (Button) myView.findViewById(R.id.mycourses);
+        myButton.setOnClickListener(this);
+        return myView;
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tabs, container, false);
+
+
+
     }
 
 
+    @Override
+    public void onClick(View view) {
+
+        Fragment fragment;
+
+            fragment = new StudentMyCourses();
+            FragmentManager fm=getFragmentManager();
+            FragmentTransaction ft=fm.beginTransaction();
+            ft.replace(R.id.defaultDashboard,fragment);
+            ft.commit();
+        }
+
+
 }
+
+
+
