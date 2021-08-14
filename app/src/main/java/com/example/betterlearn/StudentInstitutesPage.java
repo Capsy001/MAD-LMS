b6f0d48dde716f4d805c66d8667342a7fa8362dc
Capsy001 <uvenfast@gmail.com>
2021-08-15 01:21:51 +0530

    completed student-dashboard

---
+++ app/src/main/java/com/example/betterlearn/StudentInstitutesPage.java
@@ -0,0 +1,29 @@
+package com.example.betterlearn;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class StudentInstitutesPage extends Fragment {
+
+    View myButton;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.fragment_student_institutes_page, container, false);
+    }
+
+}
