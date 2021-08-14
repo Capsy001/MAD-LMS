946bd51718859227748ce2f8091ab494628f917a
Capsy001 <uvenfast@gmail.com>
2021-08-14 23:37:19 +0530

    Added announcements page, institutes page

---
+++ app/src/main/java/com/example/betterlearn/StudentInstitutes.java
@@ -0,0 +1,25 @@
+package com.example.betterlearn;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class StudentInstitutes extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_student_institutes, container, false);
+
+    }
+
+
+}
