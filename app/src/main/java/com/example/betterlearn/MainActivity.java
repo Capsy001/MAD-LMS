79e33677eb94de71f0523b4d647978708e9ddc19
Capsy <71144025+Capsy001@users.noreply.github.com>
2021-09-11 01:19:03 +0530

    Register with firebase

--- app/src/main/java/com/example/betterlearn/MainActivity.java
+++ app/src/main/java/com/example/betterlearn/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.betterlearn;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
     public void signIn(View view){
