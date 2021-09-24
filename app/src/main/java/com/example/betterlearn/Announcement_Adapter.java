971c29a46fc7120e1adc9195bc12390f6734c404
chamuditha4 <36603594+chamuditha4@users.noreply.github.com>
2021-09-24 16:23:39 +0530

    Announcements Adapter

---
+++ app/src/main/java/com/example/betterlearn/Announcement_Adapter.java
@@ -0,0 +1,63 @@
+package com.example.betterlearn;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Announcement;
+
+public class Announcement_Adapter extends ArrayAdapter<Announcement> {
+
+
+    public Announcement_Adapter(@NonNull Context context, ArrayList<Announcement> dataModalArrayList) {
+        super(context, 0, dataModalArrayList);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        // below line is use to inflate the
+        // layout for our item of list view.
+        View listitemView = convertView;
+        if (listitemView == null) {
+            listitemView = LayoutInflater.from(getContext()).inflate(R.layout.announcements_items, parent, false);
+        }
+
+        // after inflating an item of listview item
+        // we are getting data from array list inside
+        // our modal class.
+        Announcement dataModal = getItem(position);
+
+        // initializing our UI components of list view item.
+        TextView Titlelist = listitemView.findViewById(R.id.textView21_dup1);
+
+        // after initializing our items we are
+        // setting data to our view.
+        // below line is use to set data to our text view.
+        Titlelist.setText(dataModal.getTitle());
+
+
+        // below line is use to add item
+        // click listener for our item of list view.
+        listitemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // on the item click on our list view.
+                // we are displaying a toast message.
+                Toast.makeText(getContext(), "Item clicked is : " + dataModal.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        return listitemView;
+    }
+}
