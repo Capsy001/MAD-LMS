cd9d0b932320caec472ae1652fbea42bcdb5b1d5
chamuditha4 <36603594+chamuditha4@users.noreply.github.com>
2021-09-24 09:23:21 +0530

    New Announcements Updated

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 
 ## How to add image for button in android?
 ### Stackoverflow: `https://stackoverflow.com/questions/4209582/how-to-add-image-for-button-in-android`
+
+## findViewById in Fragment
+### Stackoverflow: `https://stackoverflow.com/questions/6495898/findviewbyid-in-fragment`
--- app/src/main/java/TeacherFragments/newAnnouncement.java
+++ app/src/main/java/TeacherFragments/newAnnouncement.java
@@ -2,22 +2,48 @@ package TeacherFragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.betterlearn.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class newAnnouncement extends Fragment {
 
+    public static final String TAG="TAG";
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_announcement, container, false);
+
+        View v =  inflater.inflate(R.layout.fragment_new_announcement, container, false);
+
+        Spinner spinner = (Spinner) v.findViewById(R.id.institute);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this.getActivity(), R.array.institutes, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter1);
+
+
+        return v;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
     }
 }
