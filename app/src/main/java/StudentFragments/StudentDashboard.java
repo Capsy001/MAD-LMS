762992058e14038ee79dde2de449997fbb136bea
chamalkalakshan <chamalkalakshan100@gmail.com>
2021-09-26 02:01:21 +0530

    dash changes

--- app/src/main/java/StudentFragments/StudentDashboard.java
+++ app/src/main/java/StudentFragments/StudentDashboard.java
@@ -100,3 +100,32 @@ public class StudentDashboard extends AppCompatActivity {
 
 
 }
+
+public class adminCourse extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.institutes_entroll);
+
+        Spinner spinner = (Spinner) findViewById(R.id.mycourse4);
+        spinner.setOnItemSelectedListener(this);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.mycourses, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+} */
