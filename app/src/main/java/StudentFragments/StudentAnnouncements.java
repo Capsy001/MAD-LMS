299620d8503243f34e7d22b873b32ea3ab898fc3
chamalkalakshan <chamalkalakshan100@gmail.com>
2021-09-25 17:41:41 +0530

    ui improvements

--- app/src/main/java/StudentFragments/StudentAnnouncements.java
+++ app/src/main/java/StudentFragments/StudentAnnouncements.java
@@ -22,3 +22,57 @@ public class StudentAnnouncements extends Fragment {
 
     }
 }
+
+
+
+/*@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_dashboard);
+
+        fAuth=FirebaseAuth.getInstance();
+        fStore=FirebaseFirestore.getInstance();
+
+
+        userID=fAuth.getCurrentUser().getUid();
+        DocumentReference documentReference= fStore.collection("users").document(userID);
+
+        //ddd
+        documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot value = task.getResult();
+
+
+
+                    if(value.getBoolean("type").booleanValue()==false) {
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.tabsView, new TabsTeacher())
+                                .commit();
+                    }
+                    else if(value.getBoolean("type").booleanValue()==true) {
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.tabsView, new TabsStudent())
+                                .commit();
+                    }
+
+                    if (value.exists()) {
+                        Log.d(TAG, "DocumentSnapshot data: " + value.getData());
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+        //dddd
+
+        Button logout=findViewById(R.id.logout_dashboard);
+
+        logout.setOnClickListener(view -> {
+            FirebaseAuth.getInstance().signOut();   //logout from firebase authentication
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        });
+    }*/
--- app/src/main/java/StudentFragments/StudentAssignments.java
+++ app/src/main/java/StudentFragments/StudentAssignments.java
@@ -52,3 +52,4 @@ public class StudentAssignments extends Fragment implements View.OnClickListener
     }
 }
 
+
