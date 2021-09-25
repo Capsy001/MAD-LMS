f336e8813791370286e7e48b35c1664c29f45d9d
chamuditha4 <36603594+chamuditha4@users.noreply.github.com>
2021-09-25 23:44:02 +0530

    Student Announcements

--- app/src/main/java/StudentFragments/StudentAnnouncements.java
+++ app/src/main/java/StudentFragments/StudentAnnouncements.java
@@ -2,24 +2,101 @@ package StudentFragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.Toast;
 
+import com.example.betterlearn.Announcement_Adapter;
 import com.example.betterlearn.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Announcement;
 
 
 public class StudentAnnouncements extends Fragment {
-
+    GridView coursesGV;
+    ArrayList<Announcement> dataModalArrayList;
+    FirebaseFirestore db;
+    FirebaseFirestore fStore;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
+        View myView = inflater.inflate(R.layout.fragment_student_announcements, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_announcements, container, false);
 
+        coursesGV = myView.findViewById(R.id.std_announcemets_sec);
+        dataModalArrayList = new ArrayList<>();
+
+        // initializing our variable for firebase
+        // firestore and getting its instance.
+        db = FirebaseFirestore.getInstance();
+        // here we are calling a method
+        // to load data in our list view.
+        loadDatainGridView();
+
+        return myView;
+
+    }
+
+    private void loadDatainGridView() {
+        // below line is use to get data from Firebase
+        // firestore using collection in android.
+        //firestore databse initialize
+        fStore=FirebaseFirestore.getInstance();
+        db.collection("announcements").get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        // after getting the data we are calling on success method
+                        // and inside this method we are checking if the received
+                        // query snapshot is empty or not.
+                        if (!queryDocumentSnapshots.isEmpty()) {
+                            // if the snapshot is not empty we are hiding our
+                            // progress bar and adding our data in a list.
+                            List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+                            for (DocumentSnapshot d : list) {
+
+                                // after getting this list we are passing
+                                // that list to our object class.
+                                Announcement dataModal = d.toObject(Announcement.class);
+
+                                // after getting data from Firebase
+                                // we are storing that data in our array list
+                                dataModalArrayList.add(dataModal);
+                            }
+                            // after that we are passing our array list to our adapter class.
+                            Announcement_Adapter adapter = new Announcement_Adapter(getActivity(), dataModalArrayList);
+
+                            // after passing this array list
+                            // to our adapter class we are setting
+                            // our adapter to our list view.
+                            coursesGV.setAdapter(adapter);
+                        } else {
+                            // if the snapshot is empty we are displaying a toast message.
+                            Toast.makeText(getActivity(), "No Announcements in Database", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                // we are displaying a toast message
+                // when we get any error from Firebase.
+                Toast.makeText(getActivity(), "Fail to load data..", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
 
