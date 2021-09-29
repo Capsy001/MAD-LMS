ea2fc3274caae76da249efde28fc3d127f4cb423
chamuditha4 <36603594+chamuditha4@users.noreply.github.com>
2021-09-29 22:29:04 +0530

    Announcement Edit Admin Side

--- app/src/main/java/TeacherFragments/TeacherAnnouncementsView.java
+++ app/src/main/java/TeacherFragments/TeacherAnnouncementsView.java
@@ -4,33 +4,59 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.betterlearn.R;
 
+import StudentFragments.StudentInstitutes;
+import StudentFragments.StudentInstitutesPage;
 import models.Announcement;
+import models.Institute;
 
 
-public class TeacherAnnouncementsView extends Fragment {
+public class TeacherAnnouncementsView extends Fragment implements View.OnClickListener  {
+    Button Update_announcement;
+    Announcement ancmnt2 = new Announcement();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View myView = inflater.inflate(R.layout.fragment_teacher_announcements_view, container, false);
         Bundle bundle=getArguments();
         Announcement Announce1=(Announcement) bundle.getSerializable("selected_institute1");
-
+        ancmnt2=Announce1;
         TextView title = myView.findViewById(R.id.textViewtitle);
         title.setText(Announce1.getTitle());
 
         TextView description = myView.findViewById(R.id.textViewdescription);
         description.setText(Announce1.getDescription());
 
+        Update_announcement = (Button) myView.findViewById(R.id.editbtn_annc);
+        Update_announcement.setOnClickListener(this);
 
         return myView;
 
     }
+
+    @Override
+    public void onClick(View view) {
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("key",ancmnt2); // Put anything what you want
+
+        TeacherAnnouncementsEdit fragment2 = new TeacherAnnouncementsEdit();
+        fragment2.setArguments(bundle);
+
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.defaultDashboard, fragment2)
+                .commit();
+    }
+
+
 }
 
 
