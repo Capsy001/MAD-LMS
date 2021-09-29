fa151728fe2e0077db95304257a481ae142e4d18
Capsy <71144025+Capsy001@users.noreply.github.com>
2021-09-30 05:28:48 +0530

    minor fixes

--- app/src/androidTest/java/com/example/betterlearn/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/betterlearn/ExampleInstrumentedTest.java
@@ -28,12 +28,53 @@ public class ExampleInstrumentedTest {
     public void testAverage(){
         ArrayList<Double> input=new ArrayList<Double>();
         input.add(5.0);
-        input.add(6.0);
+        input.add(7.0);
         input.add(4.0);
 
-        double result=testCalculator.getAverage(input);
+        Double result=testCalculator.getAverage(input);
 
-        assertEquals(5, result, 0);
+        assertEquals(5.33, result, 0.01);
+    }
+
+    @Test
+    public void testSum(){
+        ArrayList<Double> input=new ArrayList<Double>();
+        input.add(20.0);
+        input.add(30.4);
+        input.add(40.56);
+
+        Double result=testCalculator.getSum(input);
+        //result=Math.round(result*100.0)/100.0;
+
+
+
+
+        assertEquals(90.96, result, 0.01);
+    }
+
+
+    @Test
+    public void testMode(){
+
+        ArrayList<Double> input=new ArrayList<Double>();
+        input.add(1.0);
+        input.add(1.0);
+        input.add(1.0);
+        input.add(9.0);
+        input.add(1.0);
+        input.add(8.0);
+        input.add(9.0);
+        input.add(7.0);
+        input.add(5.0);
+        input.add(9.0);
+        input.add(2.0);
+
+        Double result=testCalculator.getMode(input);
+
+        assertEquals(1.0, result, 0);
+
+
+
     }
 
 
--- app/src/main/java/com/example/betterlearn/Calculator.java
+++ app/src/main/java/com/example/betterlearn/Calculator.java
@@ -28,6 +28,16 @@ public class Calculator extends Fragment implements View.OnClickListener {
     Button averageRst;
     TextView averageDisp;
 
+    EditText sumEdit;
+    Button sumAdd;
+    Button sumRst;
+    TextView sumDisp;
+
+    EditText modeEdit;
+    Button modeAdd;
+    Button modeRst;
+    TextView modeDisp;
+
     public Calculator() {
         // Required empty public constructor
     }
@@ -52,7 +62,7 @@ public class Calculator extends Fragment implements View.OnClickListener {
 
 
 
-
+        //Average
         averageEdit=root.findViewById(R.id.editAverage);
         averageAdd=root.findViewById(R.id.addAverage);
         averageRst=root.findViewById(R.id.resetAverage);
@@ -66,8 +76,33 @@ public class Calculator extends Fragment implements View.OnClickListener {
         averageRst.setOnClickListener(this);
 
 
+        //Sum
+        sumEdit=root.findViewById(R.id.editSum);
+        sumAdd=root.findViewById(R.id.addSum);
+        sumRst=root.findViewById(R.id.resetSum);
+        sumDisp=root.findViewById(R.id.displaySum);
+
+        sumEdit.setText("");
+        sumDisp.setText("0");
+
+        sumAdd.setOnClickListener(this);
+        sumRst.setOnClickListener(this);
 
 
+
+
+        //Average
+        modeEdit=root.findViewById(R.id.editMode);
+        modeAdd=root.findViewById(R.id.addMode);
+        modeRst=root.findViewById(R.id.resetMode);
+        modeDisp=root.findViewById(R.id.displayMode);
+
+        modeEdit.setText("");
+        modeDisp.setText("0");
+
+
+        modeAdd.setOnClickListener(this);
+        modeRst.setOnClickListener(this);
 
 
        return root;
@@ -137,5 +172,125 @@ public class Calculator extends Fragment implements View.OnClickListener {
 
 
 
+        //sum function
+        if(view==sumAdd){
+
+            double value;
+            String text =sumEdit.getText().toString();
+            if(!text.isEmpty())
+                try
+                {
+                    value= Double.parseDouble(text);
+                    // it means it is double
+
+                    sums.add(value);
+                    Double result=getSum(sums);
+
+                    sumDisp.setText(Double.toString(result));
+                    sumEdit.setText("");
+
+
+                } catch (Exception e1) {
+                    // this means it is not double
+                    e1.printStackTrace();
+                    Toast.makeText(getActivity(), "Invalid input!", Toast.LENGTH_SHORT).show();
+                }
+
+
+
+
+        }
+
+        if(view==sumRst){
+
+            sumEdit.setText("");
+            sumDisp.setText("");
+            sums.clear();
+
+
+        }
+
+
+        //Mode function
+
+        if(view==modeAdd){
+
+            double value;
+            String text =modeEdit.getText().toString();
+            if(!text.isEmpty())
+                try
+                {
+                    value= Double.parseDouble(text);
+                    // it means it is double
+
+                    modes.add(value);
+                    Double result=getMode(modes);
+
+                    modeDisp.setText(Double.toString(result));
+                    modeEdit.setText("");
+
+
+                } catch (Exception e1) {
+                    // this means it is not double
+                    e1.printStackTrace();
+                    Toast.makeText(getActivity(), "Invalid input!", Toast.LENGTH_SHORT).show();
+                }
+
+
+
+
+        }
+
+        if(view==modeRst){
+
+            modeEdit.setText("");
+            modeDisp.setText("");
+            modes.clear();
+
+
+        }
+
+
+
+
+    }
+
+    public Double getMode(ArrayList<Double> modes) {
+
+        Double mode = modes.get(0);
+        int maxCount = 0;
+
+        for (int i = 0; i < modes.size(); i++) {
+
+            Double value = modes.get(i);
+
+            int count = 0;
+            for (int j = 0; j < modes.size(); j++) {
+                if (modes.get(j) == value) {
+                    count++;
+                }
+
+
+                if (count > maxCount) {
+                    mode = value;
+                    maxCount = count;
+                }
+            }
+        }
+
+            return mode;
+
+
+    }
+
+    public Double getSum(ArrayList<Double> sums) {
+
+        Double sum=0.0;
+
+        for(Double x : sums){
+            sum+=x;
+        }
+
+        return sum;
     }
 }
