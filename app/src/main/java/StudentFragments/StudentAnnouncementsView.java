e0a27388027cd901988105ef37907d05bce0a250
chamuditha4 <36603594+chamuditha4@users.noreply.github.com>
2021-09-29 15:09:28 +0530

    Announcement View Segment

---
+++ app/src/main/java/StudentFragments/StudentAnnouncementsView.java
@@ -0,0 +1,136 @@
+package StudentFragments;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.betterlearn.Announcement_Adapter;
+import com.example.betterlearn.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Announcement;
+
+
+public class StudentAnnouncementsView extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View myView = inflater.inflate(R.layout.fragment_student_announcements_view, container, false);
+
+
+        return myView;
+
+    }
+}
+
+
+
+
+
+/*@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_dashboard);
+
+        fAuth=FirebaseAuth.getInstance();
+        fStore=FirebaseFirestore.getInstance();
+
+
+        userID=fAuth.getCurrentUser().getUid();
+        DocumentReference documentReference= fStore.collection("users").document(userID);
+
+        //ddd
+        documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot value = task.getResult();
+
+
+
+                    if(value.getBoolean("type").booleanValue()==false) {
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.tabsView, new TabsTeacher())
+                                .commit();
+                    }
+                    else if(value.getBoolean("type").booleanValue()==true) {
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.tabsView, new TabsStudent())
+                                .commit();
+                    }
+
+                    if (value.exists()) {
+                        Log.d(TAG, "DocumentSnapshot data: " + value.getData());
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+        //dddd
+
+        Button logout=findViewById(R.id.logout_dashboard);
+
+
+    }*/
+
+
+
+/*    @Override
+    public void onClick(View view) {
+
+        Fragment fragment;
+
+        if(view==myButton) {
+
+            fragment = new Annoucement();
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.defaultDashboard, fragment);
+            ft.commit();
+        }
+
+    }
+} */
+
+
+/*    public void onClick(View view) {
+        Fragment fragment;
+        if(view==Add_announcement) {
+
+            fragment = new view();
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.defaultDashboard, fragment);
+            ft.commit();
+        }
+
+        if(view==Add_announcement_text) {
+
+            fragment = new view();
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.defaultDashboard, fragment);
+            ft.commit();
+
+
+        }
+    } */
